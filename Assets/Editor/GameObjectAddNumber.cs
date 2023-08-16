8da0c88d95284d29936df1a9c4afbe67de08624f
mitsuda_yumemi <>
2023-08-16 16:12:31 +0900

    連番にできる拡張を追加

---
+++ Assets/Editor/GameObjectAddNumber.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace UnityEditorExtension
+{
+    public class GameObjectAddNumber : MonoBehaviour
+    {
+        [MenuItem(Config.ExtensionMenuPath + nameof(GameObjectAddNumber))]
+
+        static void AddNumber(MenuCommand command)
+        {
+            string number = "1";
+
+            foreach (GameObject obj in Selection.gameObjects)
+            {
+                Undo.RegisterCompleteObjectUndo(obj, "Undo Add Number");
+
+                int sibling = obj.transform.GetSiblingIndex();
+
+                obj.name = string.Format("{0}{1}", obj.name + "_", (int.Parse(number) + sibling).ToString());
+            }
+        }
+    }
+}
